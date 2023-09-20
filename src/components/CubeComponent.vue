fd9628c0d71b827fbbeeb82b96de746f1dc85252
NAGATA Minoru <nagata@lean-system.co.jp>
2023-09-20 13:40:33 +0900

    Three.jsの型定義を追加し、Vueコンポーネントでの3Dキューブの実装を開始
    変更点:
    - `@types/three` の追加により、Three.jsの型定義をプロジェクトに導入。
    - `App.vue` でのデフォルトのロゴとメッセージを削除し、新しい `CubeComponent` を表示。
    - `CubeComponent.vue` を新規作成し、3Dキューブの表示とワイヤフレームの切り替え機能を実装。
    - その他の依存関係の更新。

---
+++ src/components/CubeComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div ref="rendererDom"></div>
+  <button @click="toggleWireframe">ワイヤフレーム切り替え</button>
+</template>
+
+<script lang="ts">
+import { ref, onMounted, onBeforeUnmount, Ref } from 'vue';
+import * as THREE from 'three';
+
+export default {
+  name: 'CubeComponent',
+  setup() {
+    const rendererDom: Ref<unknown> = ref(null);
+    let scene: THREE.Scene;
+    let camera: THREE.PerspectiveCamera;
+    let renderer: THREE.WebGLRenderer;
+    let cube: THREE.Mesh;
+    let isWireframe = false;
+    let animationId: number;
+
+    onMounted(() => {
+      scene = new THREE.Scene();
+      camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      (rendererDom.value as HTMLDivElement).appendChild(renderer.domElement);
+
+      const geometry = new THREE.BoxGeometry();
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+
+      camera.position.z = 5;
+
+      animate();
+    });
+
+    onBeforeUnmount(() => {
+      cancelAnimationFrame(animationId);
+    });
+
+    const animate = () => {
+      animationId = requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    };
+
+    const toggleWireframe = () => {
+      isWireframe = !isWireframe;
+      (cube.material as THREE.MeshBasicMaterial).wireframe = isWireframe;
+    };
+
+    return {
+      rendererDom,
+      toggleWireframe
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* 必要に応じてスタイルを追加 */
+</style>
