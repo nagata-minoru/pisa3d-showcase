46dc958f5f41877ea9d8a44c6d45c097736884d3
NAGATA Minoru <nagata@lean-system.co.jp>
2023-09-26 15:41:47 +0900

    gzip圧縮とパフォーマンス設定を追加

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,26 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+
+module.exports = {
   transpileDependencies: true,
   outputDir: 'docs',
   assetsDir: './',
   publicPath: './',
-})
+
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        filename: '[file].gz',
+        algorithm: 'gzip',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    ],
+
+    performance: {
+      hints: 'warning',
+      maxAssetSize: 700_000, // バイト単位での個々のアセットの最大サイズ
+      maxEntrypointSize: 1_000_000, // バイト単位でのエントリポイントの最大サイズ
+    },
+  },
+};
